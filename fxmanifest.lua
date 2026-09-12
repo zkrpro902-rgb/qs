fd4963c895d066167a329f11d84c7b37b3dab8c2
zkrpro902-rgb <zkrpro902@gmail.com>
2026-09-12 16:23:25 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Generated with Durty Cloth Tool v3.2.0.5 (https://gta.clothing)
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+author 'VisionRP'
+version '1.0'
+
+files {
+  'mp_m_freemode_01_mp_m_visionmale2_shop.meta',
+  'pedalternatevariations.meta',
+  'first_person_alternates.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_visionmale2_shop.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
+data_file 'PED_FIRST_PERSON_ALTERNATE_DATA' 'first_person_alternates.meta'
